afbcfdd2b12508a0d2d985f95c70c62549615233
Aknandan Sharma <aknandansharma@gmail.com>
2024-12-02 17:08:30 +0530

    Commit v1.01

--- cnc-project/src/components/PageRender.jsx
+++ cnc-project/src/components/PageRender.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import NavBar from "./NavBar/index"
+import Footer from "./Pages/Footer Page/index"
 
 const PageRender = () => {
   return (
     <>
         <NavBar/>
+
+
+        <Footer/>
+
     </>
   )
 }
